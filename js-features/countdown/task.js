5fa83028a5f40ea26d72b77213639ada14e878fa
Denis-Sullivan <selivanovda@gmail.com>
2020-10-13 20:07:29 +0200

    1 change

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,12 @@
 'use strict';
 
-let sec = document.getElementById("timer");
-function time() {
-  sec.textContent--
-  if (sec.textContent == 0) {
-    alert("Вы победили в конкурсе!")
-}
+let countdown = function() {
+  let timer = document.getElementById("timer");
+  timer.textContent -= 1;
+  if (timer.textContent == 0) {
+    alert("Вы победили в конкурсе!");
+    clearInterval(timerId);
+  }
 };
 
-setInterval(time, 1000);
+let timerId = setInterval(countdown, 1000);
